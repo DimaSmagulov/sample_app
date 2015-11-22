56635a0f0523ab26d7e030f9ae4cbeece5a064a2
DmytroSmagulov <dimmond1982@hotmail.com>
2015-11-22 21:58:28 +0200

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Users
 User.create!(name:  "Dima Smagulov",
              email: "dimmond1982@hotmail.com",
              password:              "123456",
@@ -18,8 +19,17 @@ User.create!(name:  "Dima Smagulov",
               activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    dima  = users(:dima)
+    archer   = users(:archer)
+    assert_not dima.following?(archer)
+    dima.follow(archer)
+    assert dima.following?(archer)
+    assert archer.followers.include?(dima)
+    dima.unfollow(archer)
+    assert_not dima.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    dima = users(:dima)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert dima.feed.include?(post_following)
+    end
+    # Posts from self
+    dima.microposts.each do |post_self|
+      assert dima.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not dima.feed.include?(post_unfollowed)
+    end
+  end
 end
